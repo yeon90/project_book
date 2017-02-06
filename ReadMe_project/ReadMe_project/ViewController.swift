1292eda775eaa288807ef304fd7267af95414417
jiyeon park <yeon900610@gmail.com>
2017-02-06 17:35:59 +0900

    storyboard_write & backlog

---
+++ ReadMe_project/ReadMe_project/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ReadMe_project
+//
+//  Created by Ji yeon Park on 2017. 2. 6..
+//  Copyright © 2017년 Jiyeon park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //  var imageText : UITextField
+  //  var mainText : UITextField
+    
+  //  required init?(coder aDecoder: NSCoder) {
+  //      imageText = UITextField(frame: CGRect(x: 50, y: 300, width: 300, height: 150))
+  //      mainText = UITextField(frame: CGRect(x: 10, y: 500, width: 300, height: 150))
+  //      super.init(coder: aDecoder)
+  //      imageText.backgroundColor = UIColor.gray
+  //  }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+  //     view.addSubview(imageText)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
